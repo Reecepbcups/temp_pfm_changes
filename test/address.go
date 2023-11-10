c25904cbc868e8d0cc32b5fca6e6fdcf2fc23e02
Reece Williams <reecepbcups@gmail.com>
2023-11-10 09:17:02 -0600

    ibc-apps

--- test/address.go
+++ test/address.go
@@ -3,9 +3,10 @@ package test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
 )
 
 // AccAddress returns a random account address
@@ -16,6 +17,7 @@ func AccAddress() sdk.AccAddress {
 }
 
 func AccAddressFromBech32(t *testing.T, addr string) sdk.AccAddress {
+	t.Helper()
 	a, err := sdk.AccAddressFromBech32(addr)
 	require.NoError(t, err)
 
